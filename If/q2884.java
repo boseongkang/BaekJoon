12990e22d0f22e7e9d32219f5e6c0ab364aa1b61
Boseong Kang <okboseong@gmail.com>
2019-08-08 23:36:03 +0900

    if문 2884번 문제

---
+++ If/q2884.java
@@ -0,0 +1,26 @@
+package If;
+
+import java.util.Scanner;
+
+public class q2884 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+
+		int hour = h;
+		int minute = m - 45;
+		if (minute < 0) {
+			minute += 60;
+			hour--;
+			if (hour < 0) {
+				hour += 24;
+			}
+		}
+		System.out.println(hour + " " + minute);
+
+	}
+
+}
