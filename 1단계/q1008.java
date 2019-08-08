cf329113607ecd4574088b0c343681bb0f829ff4
Boseong Kang <okboseong@gmail.com>
2019-08-09 00:48:27 +0900

    1008

---
+++ 1단계/q1008.java
@@ -0,0 +1,17 @@
+package step1;
+
+import java.util.Scanner;
+
+public class q1008 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		double result = A / B;
+		System.out.println(result);
+	}
+
+}
