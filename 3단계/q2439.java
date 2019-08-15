ea1a5dcbc5c79cbea0286dd0387a66dc4a96a326
Boseong Kang <okboseong@gmail.com>
2019-08-16 00:03:55 +0900

    2439

---
+++ 3단계/q2439.java
@@ -0,0 +1,21 @@
+package step3;
+
+import java.util.Scanner;
+
+public class q2439 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n-i-1; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i+1; k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
