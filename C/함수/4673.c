83ecc0607fb9d6f03a5a597b8df5c9f842928c7d
okboseong <okboseong@gmail.com>
2020-09-23 17:46:29 +0900

    change name

---
+++ C/함수/4673.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+int sum_num(int num) {
+
+	int new_num = num;
+
+	while (num > 0) {
+		new_num += num % 10;
+		num /= 10;
+	}
+	return new_num;
+
+}
+int main(void) {
+
+	int arr[10001], cnt;
+	for (int i = 1; i < 10001; i++) {
+		cnt = sum_num(i);
+		if (cnt < 10001)
+			arr[cnt] = 1;
+
+	}
+	for (int i = 1; i < 10001; i++) {
+		if (arr[i] != 1)
+			printf("%d\n", i);
+	}
+	return 0;
+
+}
+
